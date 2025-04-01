7dbf08414a8cd46106e1c472ef5438c190facbd5
Amr Tarek <121955814+amrtarek97@users.noreply.github.com>
2025-04-01 16:37:19 +0200

    display relevant todos to open tab

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,11 +2,17 @@ import { TodoCard } from "./TodoCard";
 
 export function TodoList(props) {
     const {todos} = props;
-    const tab = 'All';
+    const tab = 'Open';
+    const filterTodoList = tab ==='All' ?
+    todos :
+    tab ==='Completed' ? 
+        todos.filter((val) => val.complete) :
+    todos.filter((val)=> !(val.complete));
+
 
     return (
         <>
-            {todos.map((todo, todoIndex)=> {
+            {filterTodoList.map((todo, todoIndex)=> {
                 return (
                     <TodoCard key={todoIndex} todoIndex={todoIndex} {...props} />
                 )
